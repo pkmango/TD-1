e5579d067be8dd2020b6c46f5ddf517a5ee7ffd6
pkmango <pk-man@yandex.ru>
2020-05-06 20:41:33 +0300

    First commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public List<Vector2> wayPoints;
+    public float speed;
+
+    private Vector2 currentPosition;
+    private float progress = 0f;
+    private int currentPoint = 0;
+    private Vector2 direction;
+
+
+    void Start()
+    {
+        currentPosition = transform.position;
+        //Debug.Log(mylist);
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (wayPoints != null)
+        {
+            transform.position = Vector2.Lerp(currentPosition, wayPoints[currentPoint], progress);
+        }
+        
+
+        if(progress < 1f)
+        {
+            progress += Time.deltaTime * speed;
+        }
+        else
+        {
+            if((wayPoints.Count - 1) == currentPoint)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            progress = 0f;
+            currentPosition = wayPoints[currentPoint];
+            currentPoint++;
+            ChangeRotation();
+        }
+        
+    }
+
+    void ChangeRotation()
+    {
+        direction = wayPoints[currentPoint] - currentPosition;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+}
---
+++ Assets/Scripts/TilemapController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapController : MonoBehaviour
+{
+
+    private Tilemap map;
+
+    void Start()
+    {
+        map = GetComponent<Tilemap>();
+        //Debug.Log(map.tileAnchor);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
