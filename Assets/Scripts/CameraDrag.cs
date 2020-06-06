fcafc8fd64121526eb299e92bf117e0efbfefd17
pkmango <pk-man@yandex.ru>
2020-06-06 15:04:57 +0300

    New CameraDrag

--- Assets/Scripts/CameraDrag.cs
+++ Assets/Scripts/CameraDrag.cs
@@ -1,45 +1,81 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
+﻿using UnityEngine;
 
-public class CameraDrag : MonoBehaviour, IDragHandler
+public class CameraDrag : MonoBehaviour//, IDragHandler, IBeginDragHandler
 {
-    private Vector3 startPosition;
+    public float startPositionY = 2f; // Начальная координат y для камеры
+
     private float deltaMax;
     private float deltaMin;
     private float deltaLimit; // Предел смещения камеры по y
     private float maxOrthSize = 10f; // Максимальный размер orthographicSize
+    private float screenLimit; // Нижняя граница для drag
 
-    void Start()
+    bool allowDrag = false;
+
+    // The key we will use for dragging
+    public KeyCode dragKey = KeyCode.Mouse0;
+
+    // The ground plane that we will drag along
+    // is defined by an origin point and a normal
+    public Vector3 groundOrigin = Vector3.zero;
+    public Vector3 groundNormal = Vector3.up;
+
+    Plane _groundPlane;
+    Vector3 _dragOrigin;
+
+    // We'll save references to the components
+    // which we'll need repeatedly
+    Camera _camera;
+    Transform _transform;
+
+    public void Start()
     {
-        startPosition = transform.position;
+        // initialisation
+        _camera = GetComponent<Camera>();
+        _transform = GetComponent<Transform>();
+        _groundPlane = new Plane(groundNormal, groundOrigin);
+
+        screenLimit = Screen.height / Camera.main.orthographicSize * 2f;
+
         deltaLimit = maxOrthSize - Camera.main.orthographicSize;
         // Если лимит меньше четверти ячейки то нет смысла делать drag
         if (deltaLimit < 0.25f) deltaLimit = 0f;
-        deltaMax = Camera.main.transform.position.y + deltaLimit;
-        deltaMin = Camera.main.transform.position.y - deltaLimit;
-        Debug.Log(deltaMax);
-        Debug.Log(deltaMin);
+        deltaMax = startPositionY + deltaLimit;
+        deltaMin = startPositionY - deltaLimit;
     }
 
-    //public void OnBeginDrag(PointerEventData eventData)
-    //{
-    //    //Debug.Log(Input.mousePosition);
-    //    //Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-    //}
-
-    public void OnDrag(PointerEventData eventData)
+    public void Update()
     {
-        Vector3 oldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition - new Vector3(eventData.delta.x, eventData.delta.y, 0f));
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 deltaPos = mousePos - oldPos;
-        Camera.main.transform.position = new Vector3(0f, Mathf.Clamp(Camera.main.transform.position.y - deltaPos.y, deltaMin, deltaMax), -10f);   
-    }
+        float distanceToIntersection;
+        Ray mouseRay = _camera.ScreenPointToRay(Input.mousePosition);
+        
+        // start drag
+        if (Input.GetKeyDown(dragKey))
+        {
+            if (Input.mousePosition.y > screenLimit)
+            {
+                allowDrag = true;
+                _groundPlane.Raycast(mouseRay, out distanceToIntersection);
+                _dragOrigin = mouseRay.GetPoint(distanceToIntersection);
+            }
+            else
+            {
+                allowDrag = false;
+            }
+            
+        }
 
-    //public void OnEndDrag(PointerEventData eventData)
-    //{
-    //    //transform.position = startPosition;
-    //    //Debug.Log("энд драг");
-    //}
+        if (allowDrag)
+        {
+            // continue drag
+            if (Input.GetKey(dragKey))
+            {
+                _groundPlane.Raycast(mouseRay, out distanceToIntersection);
+                Vector3 intersection = mouseRay.GetPoint(distanceToIntersection);
+                //_transform.position += new Vector3(0f, _dragOrigin.y - intersection.y, 0f);
+                Vector3 deltaPos = _dragOrigin - intersection;
+                _transform.position = new Vector3(0f, Mathf.Clamp(_transform.position.y + deltaPos.y, deltaMin, deltaMax), -10f);
+            }
+        }
+    }
 }
